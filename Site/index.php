933952223e2022ad9ed101faf68494cb24a88965
Philip Gilbreth <pgilbreth@gmail.com>
2016-04-18 12:18:22 -0500

    Added Business People

--- Site/index.php
+++ Site/index.php
@@ -11,18 +11,17 @@ if ($_SESSION["loggedin"] == "true") {
     header("Location: home.php");
 }
 ?>
+    <div class="row" style="margin-top: 0em">
+        <img src="backgrounds/bg<?php echo(rand(0,16));?>.jpg" style="height: 40em; width: 70em"/>
+    </div>
     <div class="row">
-        <div class="col-lg-6">
-            <h1 class="brand">Link-Book</h1>
+        <div class="col-lg-7">
+            <h1 class="brand" style="padding-left: 0px">Link-Book</h1>
         </div>
         <div class="col-lg-2"></div>
         <div class="col-lg-1" style="margin-top: 20px">
             <button class="btn btn-success" style="padding: 10px; padding-left: 20px; padding-right: 20px" onclick="window.location.href='register.php'">Sign-Up</button>
         </div>
-    </div>
-    <div class="row" style="margin-top: 1em">
-        <img src="http://il5.picdn.net/shutterstock/videos/6875158/thumb/1.jpg"/>
-    </div>
 </div>
 
 </body>
