b9072ecf896a66f0edc66cac0bc0fe144fcb4670
Philip Gilbreth <pgilbreth@gmail.com>
2016-04-19 21:22:09 -0500

    Finished Styling Profile

--- Site/navbar.php
+++ Site/navbar.php
@@ -7,7 +7,7 @@ echo("<nav class=\"navbar navbar-default navbar-fixed-top\">
         <div id='navbar' class=\"navbar-collapse collapse\">
           <ul class=\"nav navbar-nav\">
             <li id='homeTab'><a href=\"home.php\">Home</a></li>
-            <li id='profileTab'><a href=\"profile.php\">Profile</a></li>
+            <li id='profileTab'><a href=\"profile.php?uid=$_SESSION[uid]\">Profile</a></li>
             <li id='connectionsTab'><a href=\"connections.php\">Connections</a></li>
             <li id='searchTab'><a href=\"search.php\">Search</a></li>
             <li id='listingsTab'><a href=\"listings.php\">Listings</a></li>
