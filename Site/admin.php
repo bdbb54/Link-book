a011c97ade96d39e40412fafacbe7fdd17f0ba2c
Kenrick Niedbalski <kjnzr3@mail.missouri.edu>
2016-04-26 00:32:06 -0500

    Added admin.php

---
+++ Site/admin.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+    <title>Linkbook &mdash; Administration</title>
+    <?php include("header.php");?>
+</head>
+<body>
+<?php
+include("checksession.php");
+include("navbar.php");
+include("profileController.php");
+?>
+<div>
+    <h1> Welcome Admin
+</div>
+
+
+
+</body>
+</html>
