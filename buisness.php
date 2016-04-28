25e820101db4e2b7ec4d61828e2ebeff5994cdac
schnoibie <davidjschneider@mizzou.edu>
2016-04-27 22:26:55 -0500

    BuisnessPage
    Added Buisness page and its controller.
    Not done yet. Needs a couple more functions

---
+++ buisness.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+
+
+<html>
+<head>
+    <title>Link-Book: Buisness</title>
+    <?php include("header.php");?>
+</head>
+<body>
+<div class="container">
+
+    <?php
+    include("checksession.php");
+    include("navbar.php");
+    include("buisnessController.php");
+    ?>
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    </div>
+    
+</html>    
